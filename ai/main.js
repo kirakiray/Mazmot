c419b4ec45213393aebda1d9f2127f021966250f
Yao <kirayao@hotmail.com>
2026-05-04 12:12:09 +0800

    feat: 实现AI供应商助手基础功能

--- ai/main.js
+++ ai/main.js
@@ -1,4 +1,14 @@
 import { storage } from "/gh/kirakiray/ever-cache/src/main.js";
+import DeepseekAssistant from "./supplier/deepseek.js";
+
+// 保存所有api key的数据
+export const apiKeys = $.stanz([]);
+
+// 数据发生改动后，更新storage
+apiKeys.watchTick(() => {
+  const data = apiKeys.toJSON();
+  storage.apiKeys = data;
+});
 
 /**
  * 保存key
@@ -6,15 +16,42 @@ import { storage } from "/gh/kirakiray/ever-cache/src/main.js";
  * @param {*} provider 供应商
  */
 export const saveKey = async (apiKey, provider) => {
-    const keys = (await storage.apiKeys) || [];
-    keys.push({ provider, apiKey });
-    storage.apiKeys = keys;
+  const id = Math.random().toString(36).slice(2);
+
+  apiKeys.push({
+    id,
+    concurrent: 4,
+    provider,
+    apiKey,
+    createdAt: new Date().toISOString(),
+  });
+
+  return getAssistant(id);
 };
 
 /**
- * 获取所有保存的keys
- * @returns {Array} 返回所有供应商的api key数组
+ * 获取 assistant
+ * 如果不提供id，则随机获取助手
+ * @param {*} id key id
+ * @returns {Promise} 返回 assistant
  */
-export const getAllKeys = async () => {
-    return (await storage.apiKeys) || [];
+export const getAssistant = async (id) => {
+  let item;
+
+  if (!id) {
+    // 从apiKeys中随机获取一个key
+    item = apiKeys[Math.floor(Math.random() * apiKeys.length)];
+  } else {
+    item = apiKeys.find((item) => item.id === id);
+    if (!item) {
+      throw new Error("key not found");
+    }
+  }
+
+  switch (item.provider) {
+    case "deepseek":
+      return new DeepseekAssistant(item.id, item.apiKey);
+    default:
+      throw new Error("provider not supported");
+  }
 };
--- ai/supplier/deepseek.js
+++ ai/supplier/deepseek.js
@@ -1,8 +1,45 @@
-// 调用deepseek api 聊天
-export const chat = ({ message, apiKey }) => {};
+// // 调用deepseek api 聊天
+// export const chat = ({ message, apiKey }) => {};
 
-// 获取可用的模型
-export const getModels = (apiKey) => {};
+// // 获取可用的模型
+// export const getModels = (apiKey) => {};
 
-// 查询剩余调用次数和过期时间
-export const getRemaining = (apiKey) => {};
+// // 查询剩余调用次数和过期时间
+// export const getRemaining = (apiKey) => {};
+
+import { Assistant } from "./assistant.js";
+
+export class DeepseekAssistant extends Assistant {
+  id;
+  constructor(id, apiKey) {
+    super(id, apiKey);
+  }
+
+  /**
+   * 调用deepseek api 聊天
+   * @param {*} message 消息
+   * @param {*} think 是否思考思考过程
+   * @returns {Promise} 返回聊天结果
+   */
+  async chat({ message, think = false }) {
+    return await this.api.chat({ message, apiKey: this.apiKey });
+  }
+
+  /**
+   * 获取可用的模型
+   * @returns {Promise} 返回模型列表
+   */
+  async getModels() {
+    return await this.api.getModels({ apiKey: this.apiKey });
+  }
+
+  /**
+   * 查询剩余调用次数和过期时间
+   * @returns {Promise} 返回剩余调用次数和过期时间
+   */
+  async getRemaining() {
+    return await this.api.getRemaining({ apiKey: this.apiKey });
+  }
+}
+
+export default DeepseekAssistant;
