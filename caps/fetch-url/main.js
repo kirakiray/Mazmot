454a80885624020d2b1541286c2dc64a74471b8b
Yao <kirayao@hotmail.com>
2026-05-10 15:27:29 +0800

    fix(cap-request): 调整样式类名和字体样式

--- caps/fetch-url/main.js
+++ caps/fetch-url/main.js
@@ -1,5 +1,9 @@
 export default async function fetchUrl(options) {
-  const { url, maxSize, cleanHTML = true } = options;
+  const {
+    url,
+    maxSize = 32 * 1024, // 32KB
+    cleanHTML = true, // 如果遇到是html内容，是否清理多余标签，只返回包含文本的内容
+  } = options;
 
   const response = await fetch(url);
 
@@ -9,19 +13,21 @@ export default async function fetchUrl(options) {
 
   let data = await response.text();
 
-  if (maxSize && data.length > maxSize) {
-    data = data.substring(0, maxSize);
-  }
-
   if (cleanHTML) {
     const bodyMatch = data.match(/<body[^>]*>([\s\S]*?)<\/body>/i);
-    const bodyContent = bodyMatch ? bodyMatch[1] : data;
-
-    debugger;
+    let bodyContent = bodyMatch ? bodyMatch[1] : data;
+    bodyContent = bodyContent.replace(/<script[^>]*>[\s\S]*?<\/script>/gi, "");
+    bodyContent = bodyContent.replace(/<svg[^>]*>[\s\S]*?<\/svg>/gi, "");
+    bodyContent = bodyContent.replace(/<style[^>]*>[\s\S]*?<\/style>/gi, "");
 
     const template = document.createElement("template");
     template.innerHTML = bodyContent;
-    data = template.content.textContent.trim();
+    data = template.innerHTML;
+    debugger;
+  }
+
+  if (maxSize && data.length > maxSize) {
+    data = data.substring(0, maxSize);
   }
 
   return data;
